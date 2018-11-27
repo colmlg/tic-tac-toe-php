2854be7380c40ec5ab5fc70ce5d50df757ec0612
Colm Le Gear <colm.legear@gmail.com>
2018-11-27 21:13:59 +0000

    Added leaderboard

--- leaderboard.php
+++ leaderboard.php
@@ -1,16 +1,85 @@
 <?php
+include 'soapclient.php';
+$response = $client->leagueTable()->return;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of leaderboard
- *
- * @author David
- */
-class leaderboard {
-    //put your code here
+$games = explode("\n", $response);
+$players = array();
+foreach($games as $game) {
+    $components = explode(",", $game);
+    $player1 = $components[1];
+    $player2 = $components[2];
+    if(!in_array($player1, $players)) {
+        $players[] = $player1;
+    }
+    
+    if(!in_array($player2, $players)) {
+        $players[] = $player2;
+    }
 }
+
+$wins = array_fill(0, count($players), 0);
+$losses = array_fill(0, count($players), 0);
+$draws = array_fill(0, count($players), 0);
+
+foreach($games as $game) {
+    $components = explode(",", $game);
+    $params = array(
+        'gid' => $components[0]
+    );
+    $result = $client->getGameState($params)->return;
+            switch ($result) {
+                case 1:
+                    $index = array_search($components[1], $players);
+                    $wins[$index] += 1;
+                    $index = array_search($components[2], $players);
+                    $losses[$index] += 1;
+                    break;
+                case 2:
+                    $index = array_search($components[2], $players);
+                    $wins[$index] += 1;
+                    $index = array_search($components[1], $players);
+                    $losses[$index] += 1;
+                    break;
+                case "3":
+                    $index = array_search($components[1], $players);
+                    $draws[$index] += 1;
+                    $index = array_search($components[2], $players);
+                    $draws[$index] += 1;
+                default:
+            }
+}
+
+?>
+<html>
+    <head>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    </head>
+    <body>
+        <div class="row affix-row">
+            <?php include 'navbar.php' ?>
+            <div class="container">
+                <div class="page-header">
+                    <h3><span class="glyphicon glyphicon-th"></span> Tic Tac Toe</h3>
+                </div>
+                <h4>Leaderboard</h4>
+                <table class="table  table-striped">
+                    <thead>
+                    <th>Player</th>
+                    <th>Wins</th>
+                    <th>Losses</th>
+                    <th>Draws</th>
+                    </thead>
+                <?php 
+                    for($i = 0; $i < count($players); $i++) {
+                        echo "<tr>";
+                        echo "<td>" . $players[$i] . "</td><td>" . $wins[$i] . "</td><td>" . $losses[$i] . "</td><td>" . $draws[$i] . "</td>";
+                        echo "</tr>";
+                    }
+                ?>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
--- navbar.php
+++ navbar.php
@@ -11,7 +11,7 @@
                     </li>
                     <li><a href="newgame.php"><span class="glyphicon glyphicon-plus"></span> Create New Game</a></li>
                     <li><a href="opengames.php"><span class="glyphicon glyphicon-align-center"></span> Join Existing Game</a></li>
-                    <li><a href=""><span class="glyphicon glyphicon-signal"></span> Leaderboard</a></li>
+                    <li><a href="leaderboard.php"><span class="glyphicon glyphicon-signal"></span> Leaderboard</a></li>
                     <li><a href="mystatistics.php"><span class="glyphicon glyphicon-info-sign"></span> Your Statistics</a></li>
                 </ul>
             </div>
