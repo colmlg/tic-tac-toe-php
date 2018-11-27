ab41c701ffb458556b66e17c4b5e7782c41ec57b
David Kearney <15169235@studentmail.ul.ie>
2018-11-27 18:18:11 +0100

    Add files via upload

--- tictactoe.php
+++ tictactoe.php
@@ -28,7 +28,7 @@ table {
 
 <?php
 session_start();
-
+echo "test";
 ?>
 
 <div class="row affix-row">
@@ -63,8 +63,8 @@ session_start();
         </li>
         <li><a href="newgame.php"><span class="glyphicon glyphicon-plus"></span> Create New Game</a></li>
         <li><a href="opengames.php"><span class="glyphicon glyphicon-align-center"></span> Join Existing Game <span class="badge pull-right">42</span></a></li>
-        <li><a href=""><span class="glyphicon glyphicon-signal"></span> Leaderboard</a></li>
-        <li><a href=""><span class="glyphicon glyphicon-info-sign"></span> Your Statistics</a></li>
+        <li><a href="leaderboard.php"><span class="glyphicon glyphicon-signal"></span> Leaderboard</a></li>
+        <li><a href="mystatistics.php"><span class="glyphicon glyphicon-info-sign"></span> Your Statistics</a></li>
       </ul>
       </div><!--/.nav-collapse -->
     </div>
